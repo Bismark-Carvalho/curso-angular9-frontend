30c08954ab8dc55d84b5d396bd17d18eab151677
Bismark de Carvalho <47608746+Bismark-Carvalho@users.noreply.github.com>
2020-10-02 20:10:28 -0300

    Implementacao de Novos Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from "@angular/material/list";
 import { HomeComponent } from './views/home/home.component';
 import { MatCardModule } from "@angular/material/card";
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
 
 
 @NgModule({
@@ -21,7 +23,9 @@ import { MatCardModule } from "@angular/material/card";
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective
   ],
   imports: [
     BrowserModule,
