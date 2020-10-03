b87d84fa23c6021f85ce1722c53df9c96eb17a22
Bismark de Carvalho <47608746+Bismark-Carvalho@users.noreply.github.com>
2020-10-03 12:18:12 -0300

    Implementacao de novas funcionalidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,14 @@ import { HomeComponent } from './views/home/home.component';
 import { MatCardModule } from "@angular/material/card";
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -25,7 +32,9 @@ import { RedDirective } from './directives/red.directive';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ProductCreateComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -34,7 +43,13 @@ import { RedDirective } from './directives/red.directive';
     MatToolbarModule, 
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/product/product.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Observable } from 'rxjs';
+import { Product } from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseURL = "http://localhost:3001"
+
+  constructor(private snackbar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string): void {
+    this.snackbar.open(msg, 'Fechar', {
+      duration: 5000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+
+    })
+  }
+
+  create (product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseURL+"/products", product)
+  }
+
+}
